1605794dd794a1d66748663328da0b3137f6f4e5
aricci95 <aricci95@gmail.com>
2016-02-23 11:47:49 +0100

    correction bug JS loading gif

--- appli/inc/ajax/link.js
+++ appli/inc/ajax/link.js
@@ -2,15 +2,19 @@ $(document).ready(function() {
     $(".whiteBox").on('click', '.link', function(e) {
         e.preventDefault();
         e.stopPropagation();
+
         var link = $(e.target);
-        link.attr('src', 'images/icone/loading.gif');
         var divLink           = link.closest('.divLink');
         var destinataire      = link.closest('.linkDestinataire');
         var destinataireId    = destinataire.attr('data-destinataire-id');
         var destinatairePhoto = '';
+
+        link.attr('src', 'MLink/images/icone/loading.gif');
+
         if(destinataire.attr('data-destinataire-photo') !== '') {
             destinatairePhoto = '/MLink/photos/small/'+destinataire.attr('data-destinataire-photo');
         }
+
         var destinataireLogin = destinataire.attr('data-destinataire-login');
         var destinataireMessage  = destinataire.attr('data-destinataire-mail');
         var status      = parseInt(link.attr('data-status'));
@@ -39,23 +43,23 @@ $(document).ready(function() {
                             destinataire_login : destinataireLogin,
                             destinataire_mail : destinataireMessage,
                             status : status },
-        function(data) {
-            if(data == 500) {
-                $.gritter.add({
-                    title: 'Erreur',
-                    text:  'Une erreur est survenue',
-                    class_name : 'gritter-err'
-                });
-                link.hide();
-            } else {
-                $.gritter.add({
-                    title: destinataireLogin,
-                    text:  gritterMsg,
-                    class_name : 'gritter-'+gritterType,
-                    image : destinatairePhoto
-                });
+            function(data) {
+                if(data == 500) {
+                    $.gritter.add({
+                        title: 'Erreur',
+                        text:  'Une erreur est survenue',
+                        class_name : 'gritter-err'
+                    });
+                    link.hide();
+                } else {
+                    $.gritter.add({
+                        title: destinataireLogin,
+                        text:  gritterMsg,
+                        class_name : 'gritter-'+gritterType,
+                        image : destinatairePhoto
+                    });
 
-                divLink.html(data);
+                    divLink.html(data);
             }
         });
     });
