d0114e5b682656b7bda276d9a056a96ddfa8efaf
aricci95 <aricci95@gmail.com>
2016-02-26 11:24:51 +0100

    amélioration vue principale

--- appli/views/view.php
+++ appli/views/view.php
@@ -63,53 +63,59 @@
         <div><a href="home"><img src="<?php echo $this->headerImg ?>" /></a></div>
         <div class="intitule" align="center">Communauté métal en tous genres <span style="color:#B40404;margin-left:225px;"><?php echo 'v'.APP_VERSION ?></span></div>
         <div class="divCorps" align="left">
-<?php if($this->context->get('user_id')) : ?>
-    <div class="divBarre">
-    <table class="tableMenu">
-        <tr align="center">
-            <td><a class="aMenu" <?php echo ($this->page == 'profile') ? 'style="color:white;"' : ''; ?> href="profile/<?php echo $this->context->get('user_id'); ?>">Profil</a></td>
-            <td><a class="aMenu" <?php echo ($this->page == 'user') ? 'style="color:white;"' : ''; ?> href="user">Recherche</a></td>
-            <td><a class="aMenu" <?php echo ($this->page == 'forum') ? 'style="color:white;"' : ''; ?> href="forum">Forum</a></td>
-            <td><a class="aMenu" <?php echo ($this->page == 'sales') ? 'style="color:white;"' : ''; ?> href="sales">Ventes</a></td>
-            <td><a class="aMenu" <?php echo ($this->page == 'covoit') ? 'style="color:white;"' : ''; ?> href="covoit">Covoit'</a></td>
-            <?php if($this->context->get('role_id') >= AUTH_LEVEL_ADMIN) : ?>
-                <td><a class="aMenu" <?php echo ($this->page == 'adminnews') ? 'style="color:white;"' : ''; ?> href="adminNews">News</a></td>
-                <?php if($this->context->get('role_id') == AUTH_LEVEL_SUPERADMIN) : ?>
-                    <td><a class="aMenu" <?php echo ($this->page == 'admin') ? 'style="color:white;"' : ''; ?> href="admin">Admin</a></td>
-                <?php endif; ?>
-            <?php endif; ?>
-            <td><a class="aMenu" href="home/disconnect">Déconnecter</a></td>
-            <td align="right">
-                <a class="aMenu" href="mailbox"><img src="MLink/images/icone/message.png" /> <b><?php echo $this->context->get('new_messages'); ?></b></a>
-                <a class="aMenu" href="link/<?php echo LINK_STATUS_SENT; ?>"><img src="MLink/images/icone/link.png" /> <b><?php echo $this->context->get('links_count_received'); ?></b></a>
-                <a class="aMenu" href="link/<?php echo LINK_STATUS_ACCEPTED; ?>"><img src="MLink/images/icone/link_accepted.png" /> <b><?php echo $this->context->get('links_count_accepted'); ?></b></a>
-                <a class="aMenu" href="views"><img src="MLink/images/icone/views.gif" /> <b><?php echo $this->context->get('views'); ?></b></a>
-                <a class="aMenu" href="link/<?php echo LINK_STATUS_BLACKLIST; ?>"><img src="MLink/images/icone/link_refuse.png" /> <b><?php echo $this->context->get('links_count_blacklist'); ?></b></a>
-            </td>
-        </tr>
-    </table>
-    </div>
-<?php else : ?>
-    <div class="divBarre">
-    <form action="home/login" method="post">
-        <table class="tableMenu">
-            <tr>
-                <td>Login :</td><td><input name="user_login" size="8" /></td>
-                <td>Password :</td><td><input name="user_pwd" type="password" size="8" /></td>
-                <td><input type="submit" value="Connexion" /></td>
-                <td><input type="button" onclick="window.location.href = 'MLink/appli/inc/socialauth/station.php';" class="facebookButton" value="Via Facebook" /></td>
-                <td></td><td>Se souvenir de moi <input name="savepwd" type="checkbox" /></td>
-                <td></td><td><a href="lostpwd">Mot de passe oublié</a></td>
-                <td><a href="subscribe" >S'inscrire !</a></td>
-            </tr>
-        </table>
-    </form>
-    </div>
-<?php endif; ?>
-<?php $this->_helper->whiteBoxMainOpen(); ?>
-<?php if(!empty($this->_title)) : ?><h1><?php echo $this->_title; ?></h1><?php endif; ?>
-<?php include($this->getViewFileName()); ?>
-<?php $this->_helper->whiteBoxMainClose(); ?>
+        <?php if($this->context->get('user_id')) : ?>
+            <div class="divBarre">
+                <table class="tableMenu">
+                    <tr align="center">
+                        <td><a class="aMenu" <?php echo ($this->page == 'profile') ? 'style="color:white;"' : ''; ?> href="profile/<?php echo $this->context->get('user_id'); ?>">Profil</a></td>
+                        <td><a class="aMenu" <?php echo ($this->page == 'user') ? 'style="color:white;"' : ''; ?> href="user">Recherche</a></td>
+                        <td><a class="aMenu" <?php echo ($this->page == 'forum') ? 'style="color:white;"' : ''; ?> href="forum">Forum</a></td>
+                        <td><a class="aMenu" <?php echo ($this->page == 'sales') ? 'style="color:white;"' : ''; ?> href="sales">Ventes</a></td>
+                        <td><a class="aMenu" <?php echo ($this->page == 'covoit') ? 'style="color:white;"' : ''; ?> href="covoit">Covoit'</a></td>
+                        <?php if($this->context->get('role_id') >= AUTH_LEVEL_ADMIN) : ?>
+                            <?php /*
+                            <td><a class="aMenu" <?php echo ($this->page == 'adminnews') ? 'style="color:white;"' : ''; ?> href="adminNews">News</a></td>
+                            */ ?>
+                            <?php if($this->context->get('role_id') == AUTH_LEVEL_SUPERADMIN) : ?>
+                                <td><a class="aMenu" <?php echo ($this->page == 'admin') ? 'style="color:white;"' : ''; ?> href="admin">Admin</a></td>
+                            <?php endif; ?>
+                        <?php endif; ?>
+                        <td><a class="aMenu" href="home/disconnect">Déconnecter</a></td>
+                        <td align="right">
+                            <a class="aMenu" href="mailbox"><img src="MLink/images/icone/message.png" /> <b><?php echo $this->context->get('new_messages'); ?></b></a>
+                            <a class="aMenu" href="link/<?php echo LINK_STATUS_SENT; ?>"><img src="MLink/images/icone/link.png" /> <b><?php echo $this->context->get('links_count_received'); ?></b></a>
+                            <a class="aMenu" href="link/<?php echo LINK_STATUS_ACCEPTED; ?>"><img src="MLink/images/icone/link_accepted.png" /> <b><?php echo $this->context->get('links_count_accepted'); ?></b></a>
+                            <a class="aMenu" href="views"><img src="MLink/images/icone/views.gif" /> <b><?php echo $this->context->get('views'); ?></b></a>
+                            <a class="aMenu" href="link/<?php echo LINK_STATUS_BLACKLIST; ?>"><img src="MLink/images/icone/link_refuse.png" /> <b><?php echo $this->context->get('links_count_blacklist'); ?></b></a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        <?php else : ?>
+            <div class="divBarre">
+                <form action="home/login" method="post">
+                    <table class="tableMenu">
+                        <tr>
+                            <td>Login :</td><td><input name="user_login" size="8" /></td>
+                            <td>Password :</td><td><input name="user_pwd" type="password" size="8" /></td>
+                            <td><input type="submit" value="Connexion" /></td>
+                            <td><input type="button" onclick="window.location.href = 'MLink/appli/inc/socialauth/station.php';" class="facebookButton" value="Via Facebook" /></td>
+                            <td></td><td>Se souvenir de moi <input name="savepwd" type="checkbox" /></td>
+                            <td></td><td><a href="lostpwd">Mot de passe oublié</a></td>
+                            <td><a href="subscribe" >S'inscrire !</a></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        <?php endif;
+            $this->_helper->whiteBoxMainOpen();
+
+            if(!empty($this->_title)) echo '<h1>' . $this->_title . '</h1>';
+
+            include($this->getViewFileName());
+
+            $this->_helper->whiteBoxMainClose();
+        ?>
         </div>
     </div>
     </body>
