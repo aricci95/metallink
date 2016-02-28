73d10578e5508c7fc72893ca2dcaa7e9a102b249
Antoine Ricci <aricci95@gmail.com>
2016-02-28 23:35:47 +0100

    test implémentation git windows

---
+++ appli/controllers/SalesController.php
@@ -0,0 +1,25 @@
+<?php
+require_once ROOT_DIR.'/appli/controllers/SearchController.php';
+
+class SalesController extends SearchController
+{
+
+    protected $_JS   = array(JS_SCROLL_REFRESH, JS_ARTICLE);
+    protected $_type = 'Article';
+    protected $_searchParams = array('search_libel',
+                                     'search_categorie');
+
+    public function render()
+    {
+        parent::render();
+        $this->view->setTitle('Ventes');
+        $this->view->categories = $this->model->Article->getCategories();
+        $this->view->render();
+    }
+
+    public function renderMore()
+    {
+        parent::renderMore();
+        $this->view->getJSONResponse('article/wItems');
+    }
+}
