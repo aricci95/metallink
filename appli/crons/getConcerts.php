2a5f0effa56dc3dd8d10f5c07be46e9bdf8ed001
Antoine Ricci <aricci95@gmail.com>
2016-03-01 13:49:28 +0100

    ajout du style metal css cursor

--- appli/crons/getConcerts.php
+++ appli/crons/getConcerts.php
@@ -1,4 +1,11 @@
 <?php
+        $headers = 'From: "MetalLink"<contact.metallink@gmail.com>' . "\n";
+        $headers .='Reply-To: contact.metallink@gmail.com' . "\n";
+        $headers .='Content-Type: text/html; charset="utf-8"' . "\n";
+        $headers .='Content-Transfer-Encoding: 8bit';
+
+
+        mail('aricci95@gmail.com', 'cron get concert bootstrap', 'try to launch', $headers);
 
  header('Location: http://www.metallink.fr/cron/getConcerts');
  exit();
