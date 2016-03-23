3a52820e68a5eeeb2724eb38be70397a01295023
Antoine Ricci <aricci95@gmail.com>
2016-03-23 22:20:30 +0100

    correction liste

--- appli/views/user/wItems.php
+++ appli/views/user/wItems.php
@@ -1,3 +1,7 @@
-<?php foreach($this->elements as $element) :
-	$this->render('user/wItem', array('user' => $element));
-endforeach; ?>
+<?php if (!empty($this->elements)) : ?>
+    <div style="overflow: auto;">
+        <?php foreach($this->elements as $element) :
+            $this->render('user/wItem', array('user' => $element));
+        endforeach; ?>
+    </div>
+<?php endif; ?>
